35ae4ccd3850fd6392525dc3c1f24fe5b5bdcdc2
조상호 컴퓨터 <teha007@naver.com>
2021-09-22 00:58:05 +0900

    08 완료

--- 08_functions.php
+++ 08_functions.php
@@ -1,11 +1,52 @@
 <?php
 
-// Function which prints "Hello I am Zura"
+// 1. Function which prints "Hello I am Zura"
 
-// Function with argument
+// function hello()
+// {
+//   echo "Hello I am Zura" . '<br>';
+// }
+// hello();
+// hello();
+// hello();
 
-// Create sum of two functions
+// 2. Function with argument
+function hello($name)
+{
+  return "Hello I am $name" . '<br>';
+}
 
-// Create function to sum all numbers using ...$nums
+echo hello('Zura');
+echo hello('Brad');
 
-// Arrow functions
+// 3. Create sum of two functions
+// function sum($a, $b)
+// {
+//   return $a + $b;
+// }
+
+// echo sum(4, 5);
+
+// 4. Create function to sum all numbers using ...$nums
+// function sum(...$nums)
+// {
+
+//   $sum = 0;
+//   foreach ($nums as $num) {
+//     $sum += $num;
+//   }
+//   return $sum;
+// }
+
+// echo sum(1, 2, 3, 4, 5, 6);
+
+// 5. Arrow functions
+function sum(...$nums)
+{
+  return array_reduce(
+    $nums,
+    fn ($i1, $i2) => $i1 * $i2
+  );
+}
+
+echo sum(1, 2, 3, 4, 5, 6);
